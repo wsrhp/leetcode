12d5a2bb2a12d9f1a7f109368b5c1516b4e106b3
wsrhp <276531912@qq.com>
2021-03-17 07:15:37 +0800

    Add: MergeKSortedList

---
+++ app/src/main/java/com/wsrhp/leetcode/linkedlist/MergeKSortedList.java
@@ -0,0 +1,46 @@
+package com.wsrhp.leetcode.linkedlist;
+
+public class MergeKSortedList {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    private ListNode merge(ListNode[] lists, int l, int r) {
+        if (l == r) return lists[l];
+        if (l > r) return null;
+        int mid = (l + r) >> 1;
+        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+    }
+
+    private ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null || b== null) {
+            return a == null ? b : a;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = aPtr != null ? aPtr : bPtr;
+        return head.next;
+
+    }
+}
